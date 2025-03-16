bea4cd6fbc904d30564b32aeb80fe9b904643ea3
OMinkel <eltrombimc@gmail.com>
2025-03-16 05:40:27 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-import Helpers from './pages/Helpers';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import reactDOM from 'react-dom/client';
+import Navbar from './components/Navbar.jsx';
 import Hero from "./components/Hero.jsx";
+
 
 function App() {
   return (
     <>
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Navbar />
+    </div>
     <Hero />
     </>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+    return (
+        <nav className="bg-green-900 shadow-lg flex items-center justify-around py-3 px-32 fixed top-0 left-0 w-full z-50">
+            <Link to="/">
+            <span className="font-semibold text-lg flex items-center gap-3 text text-blue-400">
+                <img src="/assets/underfarm.jpg" className="text-6xl" />
+                <span className="font-semibold text-2x1">UnderFarm</span>
+            </span>
+            </Link>
+
+            <div className="flex items-center gap-5 text-black">
+            <Link to="/" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-lg hover:bg-green-700 transition duration-300">
+                Home
+            </Link>
+
+            <Link to="/account" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-lg hover:bg-green-700 transition duration-300">
+                Account
+            </Link>
+
+            <Link to="/helpers" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-lg hover:bg-green-700 transition duration-300">
+                Helpers
+            </Link>
+
+            <Link to="/settings" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-lg hover:bg-green-700 transition duration-300">
+                Settings
+            </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
