ab460860d30f993987b103451f0cf148ded902b3
Remhell <Endertasium@proton.me>
2025-03-16 14:55:25 -0500

    Fixing some stuff about the navbar and hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
-import reactDOM from 'react-dom/client';
-import Navbar from './components/Navbar.jsx';
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import reactDOM from "react-dom/client";
+import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
-
 
 function App() {
   return (
     <>
-    <div className="min-h-screen flex flex-col bg-gray-50">
       <Navbar />
-    </div>
-    <Hero />
+      <Hero />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-    return (
-        <nav className="bg-green-900 shadow-lg flex items-center justify-around py-3 px-32 fixed top-0 left-0 w-full z-50">
-            <Link to="/">
-            <span className="font-semibold text-lg flex items-center gap-3 text text-blue-400">
-                <img src="/logo.png" className="text-6xl" />
-                <span className="font-semibold text-2x1">UnderFarm</span>
-            </span>
-            </Link>
+  return (
+    <nav className="bg-green-900 shadow-lg flex items-center justify-around py-3 px-32 fixed top-0 left-0 w-full z-50">
+      <Link to="/">
+        <span className="font-semibold text-lg flex items-center gap-3 text text-blue-400">
+          <span className={styles.navlogo}></span>
+        </span>
+      </Link>
 
-            <div className="flex items-center gap-5 text-black">
-            <Link to="/" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-lg hover:bg-green-700 transition duration-300">
-                Home
-            </Link>
+      <div className="flex items-center gap-5 text-black">
+        <Link
+          to="/"
+          className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-lg hover:bg-green-700 transition duration-300"
+        >
+          Home
+        </Link>
 
-            <Link to="/account" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-lg hover:bg-green-700 transition duration-300">
-                Account
-            </Link>
+        <Link
+          to="/account"
+          className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-lg hover:bg-green-700 transition duration-300"
+        >
+          Account
+        </Link>
 
-            <Link to="/helpers" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-lg hover:bg-green-700 transition duration-300">
-                Helpers
-            </Link>
+        <Link
+          to="/helpers"
+          className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-lg hover:bg-green-700 transition duration-300"
+        >
+          Helpers
+        </Link>
 
-            <Link to="/settings" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-lg hover:bg-green-700 transition duration-300">
-                Settings
-            </Link>
-            </div>
-        </nav>
-    )
-}
+        <Link
+          to="/settings"
+          className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-lg hover:bg-green-700 transition duration-300"
+        >
+          Settings
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
