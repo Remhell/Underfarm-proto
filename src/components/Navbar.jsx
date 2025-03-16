70f87b811012f1e5d415324ae3feba4d09aff915
OMinkel <eltrombimc@gmail.com>
2025-03-16 18:22:25 +0100

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const Navbar = () => {
     return (
         <nav className="bg-green-900 shadow-lg flex items-center justify-around py-3 px-32 fixed top-0 left-0 w-full z-50">
             <Link to="/">
             <span className="font-semibold text-lg flex items-center gap-3 text text-blue-400">
-                <img src="/assets/underfarm.jpg" className="text-6xl" />
+                <img src="/logo.png" className="text-6xl" />
                 <span className="font-semibold text-2x1">UnderFarm</span>
             </span>
             </Link>
